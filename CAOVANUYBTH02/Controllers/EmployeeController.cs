2a3b903a226f2be89fa8afa7a073c9cee655c4ea
cao-uy <caovanuy00@gmail.com>
2023-02-22 14:05:46 +0700

    N2002lan3

---
+++ CAOVANUYBTH02/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAOVANUYBTH2.Controllers
+{
+    public class EmployeeController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+
+}
---
+++ CAOVANUYBTH02/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAOVANUYBTH2.Controllers
+{
+    public class StudentController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+
+}
